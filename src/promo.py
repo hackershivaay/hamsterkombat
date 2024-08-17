dbf7d739a5ba9ada39b00d969641847fd276e321
Igor Baluev <s-el-igor@mail.ru>
2024-08-17 23:53:26 +0500

    Update promo.py
    fix errors

--- src/promo.py
+++ src/promo.py
@@ -1,11 +1,16 @@
-import requests
+from sqlalchemy import create_engine, Column, String, Table, MetaData, update, select
+from sqlalchemy.orm import sessionmaker
 from collections import defaultdict
+from datetime import datetime
+import requests
 from src.utils import get_headers
 from src.__init__ import countdown_timer, log, hju, kng, mrh, pth, bru, read_config
-import mysql.connector
-from datetime import datetime
+
 
 config = read_config()
+
+def create_db_engine(db_config):
+    return create_engine(f"mysql+mysqlconnector://{db_config['user']}:{db_config['password']}@{db_config['host']}/{db_config['database']}")
 
 def load_promo_from_file(filename='./data/promo.txt'):
     with open(filename, 'r') as file:
@@ -17,24 +22,27 @@ def load_promo_from_file(filename='./data/promo.txt'):
     return promo_dict
 
 def load_promo_from_db(db_config):
-    connection = mysql.connector.connect(
-        host=db_config['host'],
-        user=db_config['user'],
-        password=db_config['password'],
-        database=db_config['database']
-    )
-    cursor = connection.cursor()
-    cursor.execute("SELECT content FROM records WHERE user_id IS NULL")
-    promo_codes = cursor.fetchall()
-    cursor.close()
-    connection.close()
+    engine = create_db_engine(db_config)
+    metadata = MetaData()
+    records_table = Table('records', metadata, autoload_with=engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     promo_dict = defaultdict(list)
-    for code in promo_codes:
-        code_type = code[0].split('-')[0]
-        promo_dict[code_type].append(code[0])
+    try:
+        stmt = select(records_table.c.content).where(records_table.c.user_id == None)
+        promo_codes = session.execute(stmt).fetchall()
+        for code in promo_codes:
+            code_type = code[0].split('-')[0]
+            promo_dict[code_type].append(code[0])
+    except Exception as e:
+        log(mrh + f"Database error: {e}")
+    finally:
+        session.close()
 
     return promo_dict
+
 
 def save_promo(promo_dict, filename='./data/promo.txt'):
     with open(filename, 'w') as file:
@@ -43,25 +51,25 @@ def save_promo(promo_dict, filename='./data/promo.txt'):
                 file.write(code + '\n')
 
 def update_promo_code_in_db(db_config, promo_code, user_id):
-    connection = mysql.connector.connect(
-        host=db_config['host'],
-        user=db_config['user'],
-        password=db_config['password'],
-        database=db_config['database']
-    )
-    cursor = connection.cursor()
-    update_query = """
-    UPDATE records
-    SET user_id = %s, date_sent = %s
-    WHERE content = %s
-    """
-    current_date = datetime.now().strftime('%Y-%m-%d')
-    cursor.execute(update_query, (user_id, current_date, promo_code))
-    connection.commit()
-    cursor.close()
-    connection.close()
+    engine = create_db_engine(db_config)
+    metadata = MetaData()
+    records_table = Table('records', metadata, autoload_with=engine)
 
-def redeem_promo(token):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        current_date = datetime.now().strftime('%Y-%m-%d')
+        stmt = update(records_table).where(records_table.c.content == promo_code).values(user_id=user_id, date_sent=current_date)
+        session.execute(stmt)
+        session.commit()
+    except Exception as e:
+        log(mrh + f"Database update error: {e}")
+    finally:
+        session.close()
+
+
+def redeem_promo(token, user_id):
     if config.get('read_from_db', False):
         promo_dict = load_promo_from_db(config['db_config'])
     else:
@@ -71,7 +79,6 @@ def redeem_promo(token):
         log(mrh + f"No codes available in {pth}promo.txt.")
         return
 
-    user_id = "66666666"
     if not user_id:
         log(mrh + "User ID not found.")
         return
@@ -101,7 +108,7 @@ def redeem_promo(token):
                     log(hju + f"Applied Promo {pth}{promo_code}")
                     codes.pop(0)
                     if config.get('read_from_db', False):
-                        update_promo_code_in_db(config['db_config'], promo_code, user_id='66666666')
+                        update_promo_code_in_db(config['db_config'], promo_code, user_id=user_id)
                     else:
                         save_promo(promo_dict)
                     countdown_timer(5)
@@ -113,19 +120,42 @@ def redeem_promo(token):
                     save_promo(promo_dict)
 
             except requests.exceptions.HTTPError as e:
-                log(kng + f"Error applying {pth}{promo_code}")
-                if config.get('read_from_db', False):
-                    update_promo_code_in_db(config['db_config'], promo_code, user_id='66666666')
-                http_error_tracker[code_type] += 1
-                if http_error_tracker[code_type] >= max_http_errors:
-                    log(pth + f"{code_type} {hju}Assuming maximum redemption")
-                    codes.pop(0)
-                    save_promo(promo_dict)
-                    attempts_tracker[code_type] = max_attempts
+                try:
+                    error_data = res.json()
+                    if error_data.get('error_code') == "MaxKeysReceived":
+                        log(pth + f"{code_type} {hju}Max keys received for today.")
+                        attempts_tracker[code_type] = max_attempts
+                    else:
+                        log(kng + f"Error applying {pth}{promo_code}: {error_data.get('error_message')}")
+                        http_error_tracker[code_type] += 1
+                        if http_error_tracker[code_type] >= max_http_errors:
+                            log(pth + f"{code_type} {hju}Assuming maximum redemption")
+                            codes.pop(0)
+                            if config.get('read_from_db', False):
+                                update_promo_code_in_db(config['db_config'], promo_code, user_id=user_id)
+                            else:
+                                save_promo(promo_dict)
+                            attempts_tracker[code_type] = max_attempts
+                except ValueError:
+                    log(kng + f"Error applying {pth}{promo_code}")
+                    http_error_tracker[code_type] += 1
+                    if http_error_tracker[code_type] >= max_http_errors:
+                        log(pth + f"{code_type} {hju}Assuming maximum redemption")
+                        codes.pop(0)
+
+                        if config.get('read_from_db', False):
+                            update_promo_code_in_db(config['db_config'], promo_code, user_id=user_id)
+                        else:
+                            save_promo(promo_dict)
+                        attempts_tracker[code_type] = max_attempts
+
             except Exception as err:
                 log(mrh + f"Error: {err}. Promo code: {promo_code}")
                 codes.pop(0)
-                save_promo(promo_dict)
+                if config.get('read_from_db', False):
+                    update_promo_code_in_db(config['db_config'], promo_code, user_id=user_id)
+                else:
+                    save_promo(promo_dict)
 
         if all(attempts >= max_attempts or not codes for attempts, codes in zip(attempts_tracker.values(), promo_dict.values())):
             break
