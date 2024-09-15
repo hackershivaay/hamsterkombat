a959b6bf449f43679ffc7f79dac0eec37a855538
Hacker Shivaay <163835685+hackershivaay@users.noreply.github.com>
2024-09-15 15:27:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
-from flask import Flask
-import subprocess
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
-
-@app.route('/run-setup')
-def run_setup():
-    try:
-        result = subprocess.run(['python', 'main.py', '--setup', 'mysetup'], capture_output=True, text=True)
-        return f"Command executed successfully: {result.stdout}"
-    except Exception as e:
-        return f"An error occurred: {str(e)}"
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
