d51ee28a7c8bea469cb84fd91143e7d593d43319
Hacker Shivaay <163835685+hackershivaay@users.noreply.github.com>
2024-09-15 20:14:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,21 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler
 import os
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
 
 TOKEN = 'YOUR_BOT_TOKEN'
 
